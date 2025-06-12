785bbec4794f7f85c5946fb22abd7a6152e7b515
Udit <25996904+uditdc@users.noreply.github.com>
2025-06-12 14:33:28 +0530

    Add health check point

--- server.ts
+++ server.ts
@@ -3,6 +3,13 @@ import plugin, { Options } from "./src/index";
 
 const app = fastify({ logger: true });
 
+// Add health check endpoint
+app.get('/health', async () => {
+	return { 
+		status: 'ok'
+	};
+});
+
 app.register(plugin, {} as any);
 
 app.listen("3000", "0.0.0.0", (err: any, address: string) => {
